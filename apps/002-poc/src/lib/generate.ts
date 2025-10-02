64e665fb735605688274bf68c4a332b77bc02921
dogwood008 <dogwood008+github@gmail.com>
2025-10-02 15:38:36 +0900

    `/implement`

--- apps/002-poc/src/lib/generate.ts
+++ apps/002-poc/src/lib/generate.ts
@@ -1,32 +1,11 @@
-export interface UserInput { name: string; examNumber: string }
+// Delegate to the root library implementation to avoid duplication
+// Types are provided via the root d.ts where available
+import type { UserInput } from '../../../src/lib/pdf/types'
+// Import JS implementation (library-first). Vite allows fs access to repo root per config.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - importing JS module for runtime implementation
+import { generateAnswerSheetPdf as generateFromLib } from '../../../src/lib/pdf/index.js'
 
-export function generateAnswerSheetPdf(_config: any, data: UserInput): Uint8Array {
-  if (!data || typeof data.name !== 'string' || typeof data.examNumber !== 'string') {
-    throw new Error('InvalidInput')
-  }
-  const pdf = [
-    '%PDF-1.4\n',
-    '1 0 obj\n',
-    '<< /Type /Catalog /Pages 2 0 R >>\n',
-    'endobj\n',
-    '2 0 obj\n',
-    '<< /Type /Pages /Kids [3 0 R] /Count 1 >>\n',
-    'endobj\n',
-    '3 0 obj\n',
-    '<< /Type /Page /Parent 2 0 R /MediaBox [0 0 612 792] >>\n',
-    'endobj\n',
-    'xref\n',
-    '0 4\n',
-    '0000000000 65535 f \n',
-    '0000000010 00000 n \n',
-    '0000000061 00000 n \n',
-    '0000000124 00000 n \n',
-    'trailer\n',
-    '<< /Size 4 /Root 1 0 R >>\n',
-    'startxref\n',
-    '188\n',
-    '%%EOF\n'
-  ].join('')
-  return new TextEncoder().encode(pdf)
+export function generateAnswerSheetPdf(config: any, data: UserInput): Uint8Array {
+  return generateFromLib(config, data)
 }
-
